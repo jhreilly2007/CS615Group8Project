97c360f937bf0fb03446fab911d47fac89d92771
jhreilly2007 <jhreilly2007@gmail.com->
2020-03-12 18:49:07 +0000

    html modal

--- view/index.js
+++
@@ -1,7 +0,0 @@
-/// index.js
-angular.module('angularApp', [])
-  .controller('indexCtrl', function($scope) {
-   // Initialize variables
-   this.name2 = '';
-   this.greeting2 = `Hi ${this.name2}`;
-  })
