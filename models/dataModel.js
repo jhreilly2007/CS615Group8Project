8bc288590967c1bea98365ef1568bae2e8de999f
jhreilly2007 <jhreilly2007@gmail.com->
2020-03-21 21:46:09 +0000

    tidy up

--- models/dataModel.js
+++ models/dataModel.js
@@ -6,7 +6,7 @@ var Schema = mongoose.Schema;
 
 // Setup mongoDB Schema
 var DataSchema = new Schema({
-    name: {type: String, required: true, max: 100},
+    name: {type: String, required: true, max: 100},	
     email: {type: String, required: true},
     gender: {type: String, required: true},
     phone: {type: String, required: true}
